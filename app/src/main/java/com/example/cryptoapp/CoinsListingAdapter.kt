156e65e1b589607427024be7e40165d98464f8a5
Nikhil <nikhil.makkad@grappus.com>
2021-09-12 23:47:44 +0530

    added searchview

--- app/src/main/java/com/example/cryptoapp/CoinsListingAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/CoinsListingAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -13,8 +15,9 @@ import com.example.cryptoapp.model.Coin
 class CoinsListingAdapter(
     private val context: Context,
     private val listener: CoinItemClickListener
-) : RecyclerView.Adapter<CoinsListingAdapter.ViewHolder>() {
-    private var coinsList = listOf<Coin>()
+) : RecyclerView.Adapter<CoinsListingAdapter.ViewHolder>(), Filterable {
+    var coinsList: ArrayList<Coin> = ArrayList()
+    var coinsListFiltered: ArrayList<Coin> = ArrayList()
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -28,16 +31,17 @@ class CoinsListingAdapter(
     }
 
     override fun onBindViewHolder(holder: CoinsListingAdapter.ViewHolder, position: Int) {
-        holder.bindViews(coinsList[position], position)
+        holder.bindViews(coinsListFiltered[position], position)
     }
 
-    fun updateList(list: List<Coin>) {
-        coinsList = list
+    fun addData(list: List<Coin>) {
+        coinsList = list as ArrayList<Coin>
+        coinsListFiltered = list
         notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
-        return coinsList.size
+        return coinsListFiltered.size
     }
 
     inner class ViewHolder(private val binding: CoinItemBinding) :
@@ -66,5 +70,36 @@ class CoinsListingAdapter(
 
     interface CoinItemClickListener {
         fun onCoinItemClicked(coin: Coin, position: Int, view: View)
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                val charString = charSequence?.toString() ?: ""
+                if (charString.isEmpty()) coinsListFiltered = coinsList else {
+                    val filteredList = ArrayList<Coin>()
+                    coinsList
+                        .filter {
+                            (it.name!!.contains(charSequence!!)) or (it.symbol!!.contains(
+                                charSequence
+                            ))
+                        }
+                        .forEach { filteredList.add(it) }
+                    coinsListFiltered = filteredList
+
+                }
+                return FilterResults().apply { values = coinsListFiltered }
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                coinsListFiltered = if (results?.values == null)
+                    ArrayList()
+                else
+                    results.values as ArrayList<Coin>
+                notifyDataSetChanged()
+            }
+
+
+        }
     }
 }
