c43b2ba0d9b098a197ea7be3175d520d421270a4
Nikhil <nikhil.makkad@grappus.com>
2021-09-20 21:31:39 +0530

    added interceptor

--- app/src/main/java/com/example/cryptoapp/controller/network/service.kt
+++ app/src/main/java/com/example/cryptoapp/controller/network/service.kt
@@ -3,19 +3,24 @@ package com.example.cryptoapp
 import com.example.cryptoapp.model.CoinsListingResponse
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiService {
     private val BASE_URL = "https://api.coingecko.com/"
+    val logging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    val client = OkHttpClient.Builder().addInterceptor(logging).build()
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).client(client)
         .build()
         .create(Api::class.java)
 
-    fun getDetails(): Single<CoinsListingResponse> {
-        return api.getDetails("usd","market_cap_desc",100)
+     fun getDetails(): Single<CoinsListingResponse> {
+        return api.getDetails("usd", "market_cap_desc", 100)
     }
 }
